fff702b6480ec516386bf7b66edb5577500eed4d
SashaDemi <demidenkosasha1996@gmail.com>
2024-02-25 14:53:54 +0200

    add dynamic class to Transaction.js.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,10 +2,12 @@ import React from "react"
 
 export const Transaction = ({transactionItem}) => {
     // if transactionItem.amount < 0 then '-' other '+'
-    const sign = transactionItem.amount < 0 ? '-' : '+';
+    const sign = transactionItem.amount < 0 ? "-" : "+";
+    const transactionColor =  transactionItem.amount < 0 ? "minus" : "plus";
+
 
     return (
-        <li className="minus">
+        <li className={transactionColor}>
             {transactionItem.text}
             {/*Math.abs - return absolut number (if -5 => 5)*/}
             <span>{sign} $ {Math.abs(transactionItem.amount)}</span>
