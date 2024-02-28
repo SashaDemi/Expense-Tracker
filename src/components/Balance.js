f7785c84a3ba6e06e3bb746f8f13105233df1084
SashaDemi <demidenkosasha1996@gmail.com>
2024-02-28 11:26:47 +0200

    add reduce method to Balance.js. It's help summary all transaction no meter how many in has.

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalContext} from "../context/GlobalState";
 
 export const Balance = () => {
+    const {transaction} = useContext(GlobalContext);
+
+    const amounts = transaction.map(transactionItem => transactionItem.amount);
+    // reduce method put two arguments, callback function(necessary) and initial accumulator
+    // (acc += item) this mean acc = acc + item
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    console.log(amounts);
     return (
         <div>
             <h4>Your Balance</h4>
-            <h1>$0.00</h1>
+            <h1>${total}</h1>
         </div>
     )
-}
+};
